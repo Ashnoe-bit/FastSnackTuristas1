63026327d0d12739d12534abe9a173d0cb039cce
Jiang <emelyjiang43@gamail.com>
2026-09-10 16:22:37 -0500

    diseño de registro

--- src/Presentacion/Registro.java
+++ src/Presentacion/Registro.java
@@ -140,90 +140,105 @@ private void aplicarEstiloMaterial(JTextField txtNombres1, JTextField txtApellid
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Sylfaen", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(153, 0, 255));
+        jLabel1.setFont(new java.awt.Font("Sylfaen", 3, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 51, 51));
         jLabel1.setText("Registro");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 20, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, 100, -1));
 
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel2.setText("Nombres:");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 65, -1, -1));
 
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel3.setText("Apellidos:");
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 105, -1, -1));
 
-        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel4.setText("Correo:");
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 142, -1, -1));
 
+        txtNombres.setBackground(new java.awt.Color(255, 204, 204));
         txtNombres.setToolTipText("");
         txtNombres.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtNombres, new org.netbeans.lib.awtextra.AbsoluteConstraints(222, 62, 130, -1));
 
+        txtApellidos.setBackground(new java.awt.Color(255, 204, 204));
         txtApellidos.setForeground(new java.awt.Color(255, 255, 255));
         txtApellidos.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtApellidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(222, 102, 130, -1));
 
+        txtCorreo.setBackground(new java.awt.Color(255, 204, 204));
         txtCorreo.setForeground(new java.awt.Color(255, 255, 255));
         txtCorreo.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtCorreo, new org.netbeans.lib.awtextra.AbsoluteConstraints(222, 142, 130, -1));
 
+        Registrar.setBackground(new java.awt.Color(255, 255, 153));
         Registrar.setText("Registrar");
-        Registrar.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        Registrar.setBorder(javax.swing.BorderFactory.createMatteBorder(3, 3, 3, 3, new java.awt.Color(255, 51, 153)));
         Registrar.addActionListener(this::RegistrarActionPerformed);
-        getContentPane().add(Registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 390, 70, 20));
+        getContentPane().add(Registrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 390, 70, 20));
 
+        txtContra.setBackground(new java.awt.Color(255, 204, 204));
         txtContra.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(222, 262, 130, -1));
 
+        jButton1.setBackground(new java.awt.Color(255, 255, 153));
         jButton1.setText("Iniciar sesión");
-        jButton1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jButton1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 51, 153), 4));
         jButton1.addActionListener(this::jButton1ActionPerformed);
         getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 390, 90, -1));
 
+        txtConfirmarContra.setBackground(new java.awt.Color(255, 204, 204));
         txtConfirmarContra.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtConfirmarContra, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 320, 130, -1));
 
-        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel5.setText("Dirección:");
         getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 182, -1, -1));
 
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel6.setText("Usuario:");
         getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(17, 225, -1, -1));
 
+        txtDireccion.setBackground(new java.awt.Color(255, 204, 204));
         txtDireccion.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtDireccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(222, 182, 130, -1));
 
-        jCheckBox1.setForeground(new java.awt.Color(255, 255, 255));
+        jCheckBox1.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jCheckBox1.setText("Mostrar contraseña");
         jCheckBox1.addActionListener(this::jCheckBox1ActionPerformed);
-        getContentPane().add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 290, 140, -1));
+        getContentPane().add(jCheckBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 290, 150, -1));
 
-        txtConfirmar.setForeground(new java.awt.Color(255, 255, 255));
+        txtConfirmar.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         txtConfirmar.setText("Mostrar contraseña");
         txtConfirmar.addActionListener(this::txtConfirmarActionPerformed);
-        getContentPane().add(txtConfirmar, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 350, 140, -1));
+        getContentPane().add(txtConfirmar, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 350, 150, -1));
 
+        txtUsuario.setBackground(new java.awt.Color(255, 204, 204));
         txtUsuario.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.LOWERED));
         getContentPane().add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(222, 222, 130, -1));
 
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel7.setText("Contraseña:");
         jLabel7.setToolTipText("");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, -1, -1));
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 270, -1, -1));
 
-        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setFont(new java.awt.Font("Microsoft YaHei UI", 1, 12)); // NOI18N
         jLabel8.setText("Vuelva a escribir la contraseña");
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 330, -1, -1));
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 320, -1, -1));
 
+        jLabel9.setBackground(new java.awt.Color(255, 204, 204));
         jLabel9.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Registro.jpg"))); // NOI18N
         getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 450));
+
+        jLabel10.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/WhatsApp Image 2026-09-05 at 11.01.51 PM.jpeg"))); // NOI18N
+        jLabel10.setText("jLabel10");
+        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 430));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -423,6 +438,7 @@ private void aplicarEstiloMaterial(JTextField txtNombres1, JTextField txtApellid
     private javax.swing.JButton jButton1;
     private javax.swing.JCheckBox jCheckBox1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
